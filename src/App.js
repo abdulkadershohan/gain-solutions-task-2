1d9105e5893b23c46527e4f182bef561ac43a9e9
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-08-04 12:58:45 +0600

    font config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ export default function App() {
   return (
     <div className="bg-gray-100">
 
-
+      <p
+        className="font-jose"
+      >
+        lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      jose: ["Josefin Sans", "sans-serif"],
+      play: ["Playfair Display", "sans-serif"],
+    },
   },
   plugins: [],
 }
