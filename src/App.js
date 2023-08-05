c94f686555af1f279f9d78f6783d4af16d2e67f3
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2023-08-05 15:54:08 +0600

    aa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import UserRoutes from "./routes/UserRoutes";
+const UserRoutes = React.lazy(() => import("./routes/UserRoutes"));
 
 export default function App() {
   return (
     <div>
       <UserRoutes />
       <ToastContainer />
+
     </div>
   )
 }
